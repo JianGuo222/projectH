aa4d08a782b4ecdfca4e8d78f871f4641aeb0fec
Jian Guo <chalmers.chester@gmail.com>
2016-11-26 20:16:31 +1100

    homepage banner install

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Slides } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('mySlider') slider: Slides;
+  // isFirstTimeLoad: boolean;
+
+  mySlideOptions = {
+    // initialSlide: 0,
+    // loop: true
+  };
 
   constructor(public navCtrl: NavController) {
+    // this.isFirstTimeLoad = true;
+  }
 
+  onSlideChanged() {
+    // if (this.isFirstTimeLoad) {
+    //   this.isFirstTimeLoad = false;
+    // } else {
+    let currentIndex = this.slider.getActiveIndex();
+    console.log("Current index is ", currentIndex);
+    // }
   }
 
 }
